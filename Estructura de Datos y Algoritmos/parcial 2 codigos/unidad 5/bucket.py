8e5ba4cc946dfb3b89d249ce463e4cbc8c7df561
AgustinPertile <pertileagustin@gmail.com>
2025-11-16 23:56:27 -0300

    Implement multiple hash functions in bucket.py
    Added multiple hash functions: modulo, square, and extraction methods.

--- Estructura de Datos y Algoritmos/parcial 2 codigos/unidad 5/bucket.py
+++ Estructura de Datos y Algoritmos/parcial 2 codigos/unidad 5/bucket.py
@@ -16,8 +16,20 @@ class Hash:
         self.__tabla=np.full((self.__areaPrimaria+self.__overflow, colisiones), None)
         self.__arregloSecundario=np.zeros(self.__areaPrimaria+self.__overflow, dtype=int)
         
-    def hash(self, clave):
-        return clave % self.__areaPrimaria
+    def hash(self, clave):#funcion has mudulo(division)
+        return clave%self.__dimension
+
+    def hash(self, clave):#funcion hash Cuadrado Medio
+        pos=clave*clave
+        strpos=str(pos)
+        cuadradoMedio=int(strpos[-2:])
+        return cuadradoMedio%self.__dimension
+
+    def hash(self, clave):#funcion hash extraccion
+        pos=clave
+        strpos=str(pos)
+        extraccion=int(strpos[-2:])
+        return extraccion%self.__dimension
     
     def insertar(self, clave):
         pos=self.hash(clave)
